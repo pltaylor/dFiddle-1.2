abe1f222c2f1f24733c61d3543e31082e6f4a1a3
Perry Taylor <ptaylor@rcrracing.com>
2013-05-08 14:57:14 -0400

    add config.js

---
+++ App/config.js
@@ -0,0 +1,43 @@
+﻿define(function() {
+
+    var remoteServiceName = 'breeze/breeze';
+
+    function xOffset(input) {
+        return input + 250;
+    }
+    
+    function yOffset(input) {
+        return (295 - input);
+    }
+    
+    var scalingFactor = .22;
+    
+    var routes = [{
+        url: 'home',
+        moduleId: 'viewmodels/home',
+        name: 'Home',
+        visible: true,
+        caption: '<i class="icon-home"></i> Home'
+    }, {
+        url: 'frames',
+        moduleId: 'viewmodels/frames',
+        name: 'Frames',
+        visible: true,
+        caption: 'Frames'
+    }, {
+        url: 'frameAdmin',
+        moduleId: 'viewmodels/frameAdmin',
+        name: 'Frame Admin',
+        visible: false,
+        caption: '<i class="icon-plus"></i> Frame Admin',
+        settings: { admin: true }
+    }];
+
+    return {
+        remoteServiceName: remoteServiceName,
+        routes: routes,
+        scalingFactor: scalingFactor,
+        xOffset: xOffset,
+        yOffset: yOffset
+    };
+})
